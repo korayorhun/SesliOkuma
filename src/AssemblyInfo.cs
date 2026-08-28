8b41bce742f8995dae8d7b903e53e5dff9a60ed1
Koray Orhun <246248547+korayorhun@users.noreply.github.com>
2026-08-28 21:21:52 +0300

    release: v1.9.0

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -6,6 +6,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Sesli Okuma")]
 [assembly: AssemblyCompany("Koray Orhun")]
 [assembly: AssemblyCopyright("Copyright © 2026 Koray Orhun. MIT License.")]
-[assembly: AssemblyVersion("1.8.0.0")]
-[assembly: AssemblyFileVersion("1.8.0.0")]
+[assembly: AssemblyVersion("1.9.0.0")]
+[assembly: AssemblyFileVersion("1.9.0.0")]
 [assembly: ComVisible(false)]
